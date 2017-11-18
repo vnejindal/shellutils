96d38cec6d9382de5407720c05c75239aaecd99f
Vinay Jindal <vinay.jindal@gmail.com>
2017-11-18 12:27:07 +0530

    changes in perf and other script

--- mem_script.sh
+++ mem_script.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
+
+set +x
+echo 'Usage: ./mem_script.sh <pid of process> <sleep time>' 
+echo 'Logs will be created in logs directory...'
+
+HOSTNAME=`hostname`
+PROCESS_ID="$1"
+SLEEP_TIME=$2
+
+
+TS="`date| sed -e 's/ /_/g'`"
+LOG_FILE="log/${HOSTNAME}_${PROCESS_ID}_memstats_${TS}.txt"
+
+
+#exec > >(tee -i $LOG_FILE)
+#exec 2>&1
+
+mkdir -p log
+
+echo '=========================================' >> $LOG_FILE
+echo $HOSTNAME >> $LOG_FILE
+
 while true
 do
-    if ps -ef | grep -i "nuance_oem_vqa_app_mt" |grep -v grep > /dev/null
+    if ps -ef | grep -i $PROCESS_ID |grep -v grep > /dev/null
     then
-	pid=$(pidof NUANCE_OEM_VQA_APP_MT)
+	pid=$PROCESS_ID
   	Pss=$(cat /proc/$pid/smaps | grep -e "^Pss:" | awk '{print $2}'| paste -sd+ | bc)
   	Pvt=$(cat /proc/$pid/smaps | grep -e "^Private:" | awk '{print $2}'| paste -sd+ | bc)
   	Rss=$(cat /proc/$pid/smaps | grep -e "^Rss:" | awk '{print $2}'| paste -sd+ | bc)
   	ts=$(date +"%T");
-  	printf "time=%s Pss=%s rss=%s Pvt=%s\n" $ts $Pss $Rss $Pvt >> "logfile_mem"
-  	sleep 5
+  	printf "time=%s Pss=%s rss=%s Pvt=%s\n" $ts $Pss $Rss $Pvt >> $LOG_FILE
+  	sleep $SLEEP_TIME
     else
-    echo "sleep 1 sec"
-    sleep 1
+    echo "sleep $SLEEP_TIME sec"
+    sleep $SLEEP_TIME
     fi
 done
 
--- perf_script.sh
+++ perf_script.sh
@@ -8,6 +8,20 @@
 #so process it terminated before the bytes are written to file
 			#Pss=$(echo $(cat /proc/$pid/smaps) | grep -e "^Pss:" | awk '{print $2}'| paste -sd+ | bc )
 
+set -x
+
+echo 'Usage: ./perf_script.sh <pid of process> <sleep time>' 
+echo 'Logs will be created in logs directory...'
+
+HOSTNAME=`hostname`
+PROCESS_ID="$1"
+SLEEP_TIME=$2
+
+TS="`date| sed -e 's/ /_/g'`"
+LOG_FILE="log/${HOSTNAME}_${PROCESS_ID}_perfstats_${TS}.txt"
+
+mkdir -p log
+
 done=0
 cpu=0
 mem=0
@@ -15,31 +29,31 @@ rss_top=0
 time_t=0
 while [ $done -eq 0 ]
 do
-	if ps -ef | grep -i "nuance_oem_vqa_app_mt" |grep -v grep > /dev/null
+	if ps -ef | grep -i "$PROCESS_ID" |grep -v grep > /dev/null
 	then
 	done=1
 	else
-	echo "sleep 1 sec"
-	sleep 1
+	echo "sleep $SLEEP_TIME sec"
+	sleep $SLEEP_TIME
 	fi
 done
-	nuance=$(ps -ef | grep -i "nuance_oem_vqa_app_mt" |grep -v grep |  awk '{print $2}')
-	top -p $nuance -b -d10 | ( trap "" 2; awk '
-	{
-	#	if($1 == "PID")
-		{
-			command="date +%T";
-			command | getline ts
-			close(command);
-		}
-		if ( $12 == "NUANCE_OEM_VQA_" )
-		{
-			cpu=$9;
-			mem=$10;
-			rss_top=$6;
-			printf "time=%s cpu=%s mem=%s rss=%s\n",ts,cpu,mem,rss_top > "logfile"
-			#printf "time=%s cpu=%s mem=%s Rss_top=%s Pss=%s Prvt=%s rss=%s\n",ts,$9,$10,$6,$PSS,$Private,$rss > "logfile"
-		}
-	}'
-	)
+top -p $PROCESS_ID -b -d$SLEEP_TIME -H > $LOG_FILE
+
+#       nuance=$(ps -ef | grep -i "$PROCESS_ID" |grep -v grep |  awk '{print $2}')
+#	top -p $PROCESS_ID -b -d10 -H | ( trap "" 2; awk '
+#	{
+#		if($1 == "PID")
+#		{
+#			command="date +%T";
+#			command | getline ts
+#			close(command);
+#		}
+#	        cpu=$9;
+#	        mem=$10;
+#		rss_top=$6;
+#		printf "time=%s cpu=%s mem=%s rss=%s\n",ts,cpu,mem,rss_top >> $LOG_FILE
+#		#printf "time=%s cpu=%s mem=%s Rss_top=%s Pss=%s Prvt=%s rss=%s\n",ts,$9,$10,$6,$PSS,$Private,$rss >> $LOG_FILE
+##		
+#	}'
+#	)
 	
